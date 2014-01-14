0b61086bf6da870307850b2ae5548c0826b841cb
Kazuyuki Sakemi <shakeren20@gmail.com>
2014-01-14 15:57:58 +0900

    add VelocityTools's .jar file separated from Velocity's .jar file.

--- build-css-validator-war.sh
+++ build-css-validator-war.sh
@@ -14,6 +14,25 @@ if [ ! -d 2002 ]; then
     # fix for an issue with the Velocity templates
     sed -i '/Velocity.getLog/i }' ./2002/css-validator/org/w3c/css/index/IndexGenerator.java
     sed -i '/For each language, we set the context/i if(false) {' ./2002/css-validator/org/w3c/css/index/IndexGenerator.java
+
+    # add VelocityTools
+    BUILD_XML="./2002/css-validator/build.xml"
+
+    REGEXP='<available file="lib\/velocity-1.7.jar"\/>'
+    REPLACEMENT='        <available file="lib\/velocity-tools-2.0.jar"\/>'
+    sed -i "/$REGEXP/ {s/.*/&\n$REPLACEMENT/;}" $BUILD_XML
+
+    REGEXP='    <get dest="tmp\/velocity-1.7.tar.gz" src="http:\/\/www.apache.org\/dist\/velocity\/engine\/1.7\/velocity-1.7.tar.gz" usetimestamp="true"\/>'
+    REPLACEMENT='    <get dest="tmp\/velocity-tools-2.0.jar" src="http:\/\/www.apache.org\/dist\/velocity\/tools\/2.0\/velocity-tools-2.0.jar" usetimestamp="true"\/>'
+    sed -i "/$REGEXP/ {s/.*/&\n$REPLACEMENT/;}" $BUILD_XML
+
+    REGEXP='    <copy file="tmp\/velocity-1.7\/velocity-1.7.jar" tofile="lib\/velocity-1.7.jar"\/>'
+    REPLACEMENT='    <copy file="tmp\/velocity-tools-2.0.jar" tofile="lib\/velocity-tools-2.0.jar"\/>'
+    sed -i "/$REGEXP/ {s/.*/&\n$REPLACEMENT/;}" $BUILD_XML
+
+    REGEXP='        <attribute name="Class-path" value=". lib\/commons-collections-3.2.1.jar lib\/commons-lang-2.6.jar lib\/jigsaw.jar lib\/tagsoup-1.2.jar lib\/velocity-1.7.jar lib\/xercesImpl.jar lib\/xml-apis.jar lib\/htmlparser-1.3.1.jar"\/>'
+    REPLACEMENT='        <attribute name="Class-path" value=". lib\/commons-collections-3.2.1.jar lib\/commons-lang-2.6.jar lib\/jigsaw.jar lib\/tagsoup-1.2.jar lib\/velocity-1.7.jar lib\/velocity-tools-2.0.jar lib\/xercesImpl.jar lib\/xml-apis.jar lib\/htmlparser-1.3.1.jar"\/>'
+    sed -i "/$REGEXP/ {s/.*/$REPLACEMENT/;}" $BUILD_XML
 fi;
 
 # build the jar file
